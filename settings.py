238328bc9f5a7ffaf10cb3e3672ffebc4ba2b0e0
落叶止于风 <jowindon@163.com>
2022-05-01 23:09:55 +0800

    增加settings，以及历史最高分显示

---
+++ settings.py
@@ -0,0 +1,11 @@
+class settings:
+    def __init__(self):
+        self.FPS = 60
+        self.WIDTH = 500
+        self.HEIGHT = 600
+
+        self.WHITE = (255,255,255)
+        self.BLACK = (0,0,0)
+        self.GREEN = (0,255,0)
+        self.RED = (255, 0, 0)
+        self.YELLOW = (255,255,0)
